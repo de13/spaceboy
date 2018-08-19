836a2283785801e9a2cbcb5b1a13c25487d1945d
Stephane Beuret <stephane.beuret@data-essential.com>
2018-08-19 17:28:24 +0200

    improve help description

--- workshop/main.go
+++ workshop/main.go
@@ -14,10 +14,10 @@ var healthz, ready int
 
 func init() {
 	startTime = time.Now()
-	flag.IntVar(&healthz, "healthz", 120, "Delay during which the application is considered as healthy.")
-	flag.IntVar(&healthz, "h", 120, "Delay during which the application is considered as healthy.")
-	flag.IntVar(&ready, "ready", 30, "Initial delay during which application is considered as not ready.")
-	flag.IntVar(&ready, "r", 30, "Initial delay during which application is considered as not ready.")
+	flag.IntVar(&healthz, "healthz", 120, "Delay (in seconds) during which the application is considered as healthy.")
+	flag.IntVar(&healthz, "h", 120, "Delay (in seconds) during which the application is considered as healthy.")
+	flag.IntVar(&ready, "ready", 30, "Initial delay (in seconds) during which application is considered as not ready.")
+	flag.IntVar(&ready, "r", 30, "Initial delay (in seconds) during which application is considered as not ready.")
 }
 
 func uptime() time.Duration {
